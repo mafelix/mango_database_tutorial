bd0e4e98d7a8ca2809d9879459280c627c18d0ff
mafelix <felixmdev@gmail.com>
2016-03-01 17:03:23 +0000

    done up to exercise 9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :movies
+  get 'users/new'
 
+  get 'users/create'
+
+  resources :movies
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
